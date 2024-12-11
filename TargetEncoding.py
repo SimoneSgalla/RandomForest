e86df89ffe913a9f65f6e924d5181e120401f139
S1098357 <s1098357@studenti.univpm.it>
2024-12-11 17:20:36 +0100

    Target encoding solo sulla colonna src_ip

--- TargetEncoding.py
+++ TargetEncoding.py
@@ -8,20 +8,17 @@ data = []
 data2 = []
 for i in range(22):
     data.append(pd.read_csv('../Dataset2/Network_dataset_'+str(1+i)+'.csv', low_memory=False, usecols=['src_ip']))
-    data.append(pd.read_csv('../Dataset2/Network_dataset_' + str(1 + i) + '.csv', low_memory=False, usecols=['label']))
+    data2.append(pd.read_csv('../Dataset2/Network_dataset_' + str(1 + i) + '.csv', low_memory=False, usecols=['label']))
 df = pd.concat(data)
 df2 = pd.concat(data2)
 
 X = df
 y = df2
 
-    # Applica Label Encoding su eventuali colonne categoriche rimanenti
-
-    # Salva i trasformatori
 enc_auto = TargetEncoder(smooth="auto")
 X_trans = enc_auto.fit_transform(X, y)
 
-dump(X_trans, 'random_forest_model.joblib')
+pd.Series(X_trans).to_csv('TransData.csv', index=False, header=False)
 
 
 
