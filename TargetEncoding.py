c00edbcc71dd2f723b958d77e69ab878793ebb54
S1098357 <s1098357@studenti.univpm.it>
2024-12-11 11:19:06 +0100

    bo

--- TargetEncoding.py
+++ TargetEncoding.py
@@ -1,19 +1,6 @@
-import ipaddress
-import os.path
-
-from sklearn.decomposition import PCA
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
-
 # Carica il dataset
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.preprocessing import LabelEncoder
-from joblib import load
-from sklearn.feature_selection import VarianceThreshold
 from joblib import dump
 from sklearn.preprocessing import TargetEncoder
 
@@ -34,7 +21,7 @@ enc_auto = TargetEncoder(smooth="auto")
 X_trans = enc_auto.fit_transform(X, y)
 
 model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+model.fit(X_trans, y)
 
 dump(X_trans, 'random_forest_model.joblib')
 
